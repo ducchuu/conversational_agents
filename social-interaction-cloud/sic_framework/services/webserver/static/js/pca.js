88a6dfc9b06bd805c2cecf2a950aeb623ceadbd7
unknown <mikolaj.duchlinski@gmail.com>
2026-01-14 01:38:51 +0100

    random recommendation and fixes to the framework,I couldn't run it properly

--- social-interaction-cloud/sic_framework/services/webserver/static/js/pca.js
+++ social-interaction-cloud/sic_framework/services/webserver/static/js/pca.js
@@ -1,83 +1,84 @@
-/** 
- * Javascript code for the Project Conversational Agents course.
- * 
- * This code makes some basic assumptions about the interaction protocol:
- * - User always is the first to say something (by turning on the microphone)
- * - The microphone automatically is closed again when a transcript has been received (the user has said something)
- * - After receiving a transcript, the turn is given to the agent and the user cannot turn on the microphone.
- * - When the agent has said something, the turn is handed back to the user.
- * 
- * This code also makes assumptions about the names of two buttons: the 'start' and 'mic'(rophone) button.
- * Button clicks are passed on to the webserver, which passes them on to the MARBEL agent using an EIS connector.
- * 
- * Finally, on pages where there is a microphone, a footer should be present with a <p> element with id="transcript".
- * This element will be used to display the transcript received from the ASR component.
- * 
- * SocketIO is used to communicate with the server.
-*/
-
 "use strict";
 
-// Establish a WebSocket connection with the Flask server
+// Establish a WebSocket connection
 var socket = io();
 
-// Flag to keep track of whose turn it is (true --> user, false --> agent)
-// Initially, it is the agent's turn; DO NOT CHANGE here, this flag is set by EISComponent in SIC framework
-var user_turn = false;
 
-// Variable to keep track of number of recipes that fullfill criteria
+var user_turn = false;
+if (sessionStorage.getItem("user_turn_saved") === "true") {
+    console.log("Restoring user turn from previous page.");
+    user_turn = true;
+
+    sessionStorage.removeItem("user_turn_saved");
+}
+
 var recipecounter = -1;
 
-// Code for handling button elements on page
+socket.off("speech");
+window.currentUtterance = null;
+
+socket.on("speech", (text) => {
+    console.log("Agent speaking:", text);
+    window.speechSynthesis.cancel(); 
+
+    window.currentUtterance = new SpeechSynthesisUtterance(text);
+
+    console.log("Unlocking microphone (Speech Start).");
+    user_turn = true; 
+
+    sessionStorage.setItem("user_turn_saved", "true");
+
+    var micImage = document.getElementById('micimg');
+    if (micImage) {
+        micImage.src = 'static/images/mic_out.png'; 
+    }
+
+    setTimeout(() => {
+        socket.emit('event', 'SpeechDone');
+    }, 500);
+
+    window.speechSynthesis.speak(window.currentUtterance);
+});
+
+
 var elements = document.getElementsByClassName("btn");
-
-// Send button clicks to server
 var sendButtonClick = function() {
-    var name = this.getAttribute("id");
-    socket.emit('buttonClick', name); // send button name to web server
-};
 
+    var name = this.getAttribute("id");
+    if (name !== "mic") {
+        socket.emit('buttonClick', name);
+    }
+};
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', sendButtonClick, false);
 }
 
-// Dedicated event listeners for two SPECIAL BUTTONS: the 'mic' button.
-// Event listener for the mic button to toggle from off to on
-// - We need to check if button is available on the webpage, as this may not be the case, e.g. not on start webpage.
-// - The socket handler for 'transcript' event turns it off again, see below.
 var micButton = document.getElementById('mic');
-
 if (micButton) {
     micButton.addEventListener('click', function() {
         if (user_turn) {
-            document.getElementById('micimg').src  = 'static/images/mic_on.png';
+            document.getElementById('micimg').src = 'static/images/mic_on.png';
+            socket.emit('buttonClick', 'mic');
         } else {
-            alert("It is not your turn.")
+            console.log("Blocked: It is not your turn.");
+            alert("It is not your turn.");
         }
     });
 }
 
-// Event handler for successful connection
-socket.on('connect', function() {
-    console.log('Connected to the server.');
-});
+socket.on('connect', function() { console.log('Connected to server.'); });
+socket.on('connect_error', function(error) { console.log('Connection error:', error); });
+socket.on('disconnect', function() { console.log('Disconnected from server.'); });
 
-// Event handler for connection errors
-socket.on('connect_error', function(error) {
-    console.log('Connection error:', error);
-});
-
-// Event handler for disconnection
-socket.on('disconnect', function() {
-    console.log('Disconnected from the server.');
-});
-
-// Event handler for transcript event
 socket.on("transcript", (text) => {
     document.getElementById("transcript").innerHTML = text;
 });
 
 socket.on("pattern", (pattern) => {
+    if (user_turn) {
+        sessionStorage.setItem("user_turn_saved", "true");
+    }
+
     switch(pattern) {
         case "start":
             window.location.href = "start.html";
@@ -89,20 +90,22 @@ socket.on("pattern", (pattern) => {
             window.location.href = "recipe_overview.html";
             break;
         default:
-          window.location.href = "closing.html";
+            window.location.href = "closing.html";
       }
 })
 
-// Event handler for switching turns
 socket.on("set_turn", (whoseturn) => {
-    if (whoseturn=="true") {
+    if (whoseturn == "true") {
         user_turn = true;
+        sessionStorage.setItem("user_turn_saved", "true");
     } else {
         user_turn = false;
-    }
-    // If it's not the user's turn (any more), then make sure the microphone icon is mic_out
-    if (!user_turn) {
-        document.getElementById('micimg').src  = 'static/images/mic_out.png';
+        sessionStorage.removeItem("user_turn_saved");
+        
+        var micImage = document.getElementById('micimg');
+        if (micImage) {
+            micImage.src = 'static/images/mic_out.png';
+        }
     }
 })
 
@@ -111,8 +114,6 @@ socket.on("recipecounter", (number) => {
     document.getElementById("recipecounter").innerHTML = recipecounter;
 })
 
-// Example showing how to work with templates
-// Adding filters to a card deck on an HTML page
 socket.on("filters", (filterString) => {
     const filtersString = filterString.substring(1, filterString.length-1);
     if (filterString.length != 0) {
@@ -124,15 +125,6 @@ socket.on("filters", (filterString) => {
     }
 })
 
-// TODO: Show recipe cards on HTML page
-
-// TODO: After selection has been made, show a specific recipe in a card with its details
-
-// TODO: Utility function to parse string representations of arrays (e.g., "[chorizo, cheese]")
-
-
-// Create and add a single card for a filter based on the template in the HTML file
-// See also https://www.w3schools.com/tags/tag_template.asp
 function filterCard(filter) {
     let filterTemplate = document.querySelector('#filterCardTemplate');
     const card = filterTemplate.content.cloneNode(true);
